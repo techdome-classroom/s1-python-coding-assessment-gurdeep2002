c5639687494de3fad5ca6ef28a4e5f9b81c82701
gurdeep2002 <gurdeeppatel885@gmail.com>
2024-09-25 12:56:12 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -2,7 +2,7 @@ def decode_message( s: str, p: str) -> bool:
      if not pattern:
         return False
 
-    if pattern[0] == '*':
+     if pattern[0] == '*':
         if len(pattern) == 1:
             return True
         return matches(message, pattern[1:]) or (message and matches(message[1:], pattern))
