9a4c525426de54a91ad0e6ca8db832f6ed7e6bd2
gurdeep2002 <gurdeeppatel885@gmail.com>
2024-09-25 13:06:54 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -18,10 +18,10 @@ def decode_message(s:str, p:str):
             for j in range(1, m + 1):
                 if pattern[i - 1] == '?' or pattern[i - 1] == message[j - 1]:
                 # If it's a '?' or a character matches, take the diagonal value
-                dp[i][j] = dp[i - 1][j - 1]
-            elif pattern[i - 1] == '*':
+                    dp[i][j] = dp[i - 1][j - 1]
+                elif pattern[i - 1] == '*':
                 # If it's a '*', either take the value above or to the left
-                dp[i][j] = dp[i - 1][j] or dp[i][j - 1]
+                    dp[i][j] = dp[i - 1][j] or dp[i][j - 1]
 
     return dp[n][m]
 
