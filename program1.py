b3e1ccf81c090466660ba7fc9ebc3a234f20cc03
gurdeep2002 <gurdeeppatel885@gmail.com>
2024-09-25 12:46:03 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -9,26 +9,26 @@ class Solution:
         rows, cols = len(grid), len(grid[0])
     
         def dfs(r, c):
-        # If out of bounds or at water, stop the DFS
+        
             if r < 0 or c < 0 or r >= rows or c >= cols or grid[r][c] == 'W':
                 return
-        # Mark the landmass as visited by changing 'L' to 'W' or any other marker
+        
             grid[r][c] = 'W'
-        # Explore the four adjacent directions
-            dfs(r+1, c)  # Down
-            dfs(r-1, c)  # Up
-            dfs(r, c+1)  # Right
-            dfs(r, c-1)  # Left
+        
+            dfs(r+1, c)  
+            dfs(r-1, c)  
+            dfs(r, c+1) 
+            dfs(r, c-1) 
 
-    # Island count
+ 
         island_count = 0
     
-    # Iterate over every cell in the grid
+    
         for r in range(rows):
             for c in range(cols):
-                if grid[r][c] == 'L':  # Found an unvisited landmass
-                    island_count += 1  # This is a new island
-                    dfs(r, c)  # Perform DFS to mark the entire island
+                if grid[r][c] == 'L': 
+                    island_count += 1  
+                    dfs(r, c)  
     
         return island_count
 
